549a5d543f4f000e4484b8cd7f1bea22ba127374
joseVitor03 <jv681033@gmail.com>
2024-01-03 10:18:00 -0300

    a

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -34,7 +34,7 @@ function Home() {
             className={ styled.btn }
             onClick={ () => navigate('/register') }
           >
-            Criar conta
+            Criar conta test
           </button>
         </div>
       </header>
